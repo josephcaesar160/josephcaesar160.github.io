f98ee3b6baeee99b5561f90dbc9a4af2a679c45f
jcrelampagos <104569187+jcrelampagos@users.noreply.github.com>
2025-01-10 20:39:59 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,6 @@
+const div = document.querySelector("div")
+const spanHi = document.querySelector('#hi')
+const spanBye = document.querySelector('#bye')
+
+spanHi.style.color = "blue"
+spanHi.style.backgroundColor = "black"
